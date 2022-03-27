c694b7d64a2377e0d50b10d7c63869c26be477aa
OMKP <oprzydatek@sii.pl>
2022-03-27 16:21:40 +0200

    db migration

--- ApartmentRental.Core/Entities/Apartment.cs
+++ ApartmentRental.Core/Entities/Apartment.cs
@@ -3,8 +3,19 @@ namespace ApartmentRental.Core.Entities;
 public class Apartment : BaseEntity
 {
     public string? RentAmount { get; set; }
-    public int NumberOfRooms { get; set; } 
+    public int NumberOfRooms { get; set; }
     public int SquareMeters { get; set; }
     public int Floor { get; set; }
     public bool IsElevator { get; set; }
+
+    public int LandlordId { get; set; }
+    public Landlord Landlord { get; set; }
+
+    public int TenantId { get; set; }
+    public Tenant Tenant { get; set; }
+
+    public int AddressId { get; set; }
+    public Address Address { get; set; }
+    
+    public IEnumerable<Image> Images { get; set; }
 }
--- ApartmentRental.Core/Entities/Tenant.cs
+++ ApartmentRental.Core/Entities/Tenant.cs
@@ -1,6 +1,9 @@
 namespace ApartmentRental.Core.Entities;
 
-public class Tenant
+public class Tenant : BaseEntity
 {
     public Apartment Apartment { get; set; }
+
+    public int AccountId { get; set; }
+    public Account Account { get; set; }
 }
